2cc4d91822d31daecca2a60f666fd3bb5d50f9b0
Mahamure Kedar <kedarmahamura12@gmail.com>
2025-04-18 11:45:31 +0530

    footer

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
           {/* footer */}
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
-              <p>Made with RoadsideCoder by Kedar</p>
+              <p>Made by Kedar...</p>
             </div>
           </footer>
         </body>
